2ba323a1d3868266e3ec804afde2c858cf2a454e
sesamig <opensesamig@yahoo.co.jp>
2016-09-03 16:38:19 +0900

    Update assessment.js

--- assessment.js
+++ assessment.js
@@ -1,5 +1,56 @@
 (function () {
-    'use strict'; 
+    'use strict';
+    const userNameInput = document.getElementById('user-name');
+    const assessmentButton = document.getElementById('assessment');
+    const resultDivided = document.getElementById('result-area');
+    const tweetDivided = document.getElementById('tweet-area');
+
+    /**
+    * 指定した要素の子どもを全て除去する
+    * @param {HTMLElement} element HTMLの要素
+    */
+    function removeAllChildren(element) {
+        while (element.firstChild) { // 子どもの要素があるかぎり削除
+            element.removeChild(element.firstChild);
+        }
+    }
+
+    assessmentButton.onclick = () => {
+        const userName = userNameInput.value;
+        if (userName.length === 0) { // 名前が空の時は処理を終了する
+            return;
+        }
+
+        // 診断結果表示エリアの作成
+        removeAllChildren(resultDivided);
+        const header = document.createElement('h3');
+        header.innerText = '診断結果';
+        resultDivided.appendChild(header);
+
+        const paragraph = document.createElement('p');
+        const result = assessment(userName);
+        paragraph.innerText = result;
+        resultDivided.appendChild(paragraph);
+
+        // ツイートエリアの作成
+        removeAllChildren(tweetDivided);
+        const anchor = document.createElement('a');
+        const hrefValue = 'https://twitter.com/intent/tweet?button_hashtag=%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D&text='
+        + encodeURIComponent(result);
+        anchor.setAttribute('href', hrefValue);
+        anchor.className = 'twitter-hashtag-button';
+        anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
+        tweetDivided.appendChild(anchor);
+
+        twttr.widgets.load();
+    };
+
+    userNameInput.onkeydown = (event) => {
+        if (event.keyCode === 13) {
+            assessmentButton.onclick();
+        }
+    };
+
     const answers = [
         '{userName}のいいところは声です。{userName}の特徴的な声はみなを惹きつけ、心に残ります。',
         '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
